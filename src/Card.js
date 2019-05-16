0056188f07a0702dea889f13b77e7218b89f4779
Operel <oriperelman@gmail.com>
2019-05-16 17:39:06 +0300

    minor refactor

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const importAll = (r) => {
     let images = {};
-    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
     return images;
 }
 
