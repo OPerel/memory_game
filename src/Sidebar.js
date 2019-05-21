a298d9e4b012aebb2706a975790f5546b84a4c9f
Operel <oriperelman@gmail.com>
2019-05-21 22:49:07 +0300

    minor styles and content changes

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,11 +5,12 @@ const Sidebar = ({ reset }) => {
     return (
         <div className="sidebar">
             <div className="header">
-                <p>Match identical cards from different suites.</p>
+                <h2>Memory Game</h2>
+                <p>Match cards from different suites.</p>
                 <button className="btn" onClick={reset}>Reset Board</button>
             </div>
             <div className="footer">
-                <p>Made by Ori Perelman with <a href="http://deckofcardsapi.com/" target="_blank" rel="noopener noreferrer">Deck Of Cards</a>.</p>
+                <p>Made with <a href="http://deckofcardsapi.com/" target="_blank" rel="noopener noreferrer">Deck Of Cards</a>.</p>
             </div>
         </div>
     )
