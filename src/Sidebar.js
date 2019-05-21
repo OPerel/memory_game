2a7330c4256b12a3e6527a21ad5384a0f5a923e0
Operel <oriperelman@gmail.com>
2019-05-21 19:50:24 +0300

    style sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,9 +4,13 @@ import './Sidebar.css';
 const Sidebar = ({ reset }) => {
     return (
         <div className="sidebar">
-            <p>Match identical card from different suites.</p>
-            <button onClick={reset}>Reset Board</button>
-            <p>Made by Ori Perelman with a <a href="http://deckofcardsapi.com/" target="_blank" rel="noopener noreferrer">Deck Of Cards</a> API</p>
+            <div className="header">
+                <p>Match identical cards from different suites.</p>
+                <button className="btn" onClick={reset}>Reset Board</button>
+            </div>
+            <div className="footer">
+                <p>Made by Ori Perelman with <a href="http://deckofcardsapi.com/" target="_blank" rel="noopener noreferrer">Deck Of Cards</a>.</p>
+            </div>
         </div>
     )
 };
