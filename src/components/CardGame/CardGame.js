eb14a2b82efb5316a4a3ae8a7a3534ac27bf2e60
Operel <oriperelman@gmail.com>
2020-06-11 18:52:40 +0300

    Fix win announcemnt

--- src/components/CardGame/CardGame.js
+++ src/components/CardGame/CardGame.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './CardGame.css';
 
-const CardGame = (props) => {
-  return (
-    <>
+const CardGame = (props) => (
+    <div className='memory-game'>
       <div id={props.win ? 'win' : 'no-win'}>
         <h3>You Win!</h3>
       </div>
-      <div className='memory-game'>
-        {props.children}
-      </div>
-    </>
-  );
-};
+      <div className="cards">{props.children}</div>
+    </div>
+);
 
 export default CardGame;
