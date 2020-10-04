84d459d6dfe52938d0bc1f12c62b680f73dc6f22
Operel <oriperelman@gmail.com>
2020-10-04 12:19:25 +0300

    refactor entar card animation to css

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,17 +7,18 @@ import back from '../../img/back.svg'
 const Card = ({ i, url, onClick }) => {
 
   return (
-    <motion.div
-      className='card'
-      animate={{
-        x: [-500, 0],
-        y: [500 - i * 40, 0],
-        rotate: [-270, -90, -45, 0]
-      }}
-      transition={{
-        duration: 0.9,
-      }}
-    >
+    // <motion.div
+    //   className='card'
+    //   animate={{
+    //     x: [-500, 0],
+    //     y: [500 - i * 40, 0],
+    //     rotate: [-270, -90, -45, 0]
+    //   }}
+    //   transition={{
+    //     duration: 0.9,
+    //   }}
+    // >
+    <div className='card'>
       <div className='inner-card' onClick={onClick}>
         <div className='front-face'>
           <img src={url} alt='f' />
@@ -32,7 +33,8 @@ const Card = ({ i, url, onClick }) => {
           <img src={back} alt='b' />
         </motion.div>
       </div>
-    </motion.div>
+    </div>
+    // </motion.div>
   );
 };
 
